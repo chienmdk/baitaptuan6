951712e5f23547cea73c856d3841b7648d9c0de6
unknown <anhchien1q2e@gmail.com>
2023-03-20 15:42:30 +0700

    first commit

---
+++ Cau1/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	package Bailab6.Cau1;
+
+	public class Main {
+	    public static void main(String[] args) {
+	     NhanVienFullTime ft=new NhanVienFullTime();
+	     ft.tinhluong();
+	     NhanVienPartTime pt=new NhanVienPartTime();
+	     pt.tinhluong();
+	    }
+	}
